055a55e1380862f491e715bbe193d67a6fb27b06
Brandon Raitt <administrator@pxoink.net>
2014-09-01 19:19:16 -0400

    Add in tabs and slide toggles.

--- js/ice.framework.js
+++ js/ice.framework.js
@@ -14,6 +14,7 @@ requirejs.config({
 		'jquery'	:	'jquery-1.11.1.min',
 		'icebox'	:	'icebox',
 		'modernizr'	:	'modernizr.min',
+		'scrollto'	:	'jquery.scrollto.min',
 		'sticky'	:	'jquery.sticky.min',
 		'whitespace':	'jquery.removeWhitespace.min'
 	}
@@ -21,6 +22,7 @@ requirejs.config({
 
 //Declare global variables.
 var	interval	=	new Object();
+var $_GET		=	getQueryParams(document.location.search);
 
 //Require Modernizr.
 require(["modernizr.min"]);
@@ -31,6 +33,7 @@ require(["jquery"], function(jQuery) {
 	require(["collage"]);
 	require(["equalize"]);
 	require(["icebox"]);
+	require(["scrollto"]);
 	require(["sticky"], function() {
 		//When the DOM is ready.
 		jQuery('document').ready(function() {
@@ -54,7 +57,7 @@ require(["jquery"], function(jQuery) {
 	require(["whitespace"]);
 	
 	//When the DOM is ready.
-	jQuery('document').ready(function() {
+	jQuery('document').ready(function() {	
 		//For each code block.
 		jQuery('code').each(function() {
 			//Declare variables.
@@ -95,6 +98,18 @@ require(["jquery"], function(jQuery) {
 			});
 		});
 		
+		//For each dismissable object.
+		jQuery('*[data-dismiss]').each(function() {
+			//Add the dismiss class.
+			jQuery(this).addClass('dismiss');
+			
+			//On click.
+			jQuery(this).click(function() {
+				//Fade the item out.
+				jQuery(this).fadeOut(400);
+			});
+		});
+		
 		//For each gallery.
 		jQuery('.gallery').removeWhitespace().each(function() {
 			//Get the target height.
@@ -125,6 +140,27 @@ require(["jquery"], function(jQuery) {
 			
 			//On hover.
 			$this.hover(function() {
+				//Keystrokes.
+				jQuery(document).keydown(function (e) {
+					//Switch key movements.
+					switch(e.which) {
+						case 37:
+							//Slide left.
+							slide($this, 0);
+							
+							//Return false.
+							return false;
+						break;
+						case 39:
+							//Slide right.
+							slide($this, 1);
+							
+							//Return false.
+							return false;
+						break;
+					}
+				});
+				
 				//Clear the interval.
 				clearInterval(interval.rand);
 			}, function() {
@@ -137,14 +173,74 @@ require(["jquery"], function(jQuery) {
 			
 			//On slide left.
 			jQuery('.slide-left').click(function() {
+				//Slide left.
 				slide($this, 0);
 			});
 			
 			//On slide right. 
 			jQuery('.slide-right').click(function() {
+				//Slide right. 
 				slide($this, 1);
 			});
 		});
+		
+		//Impement tabs.
+		jQuery('.tabs').each(function() { 
+			//Declare variables.
+			var	$this	=	jQuery(this);
+			var	hrefs	=	'';
+			
+			//For each hrefs. 
+			$this.find('a').each(function() {
+				//Increment hrefs.
+				hrefs	+=	jQuery(this).attr('href') + ',';
+				
+				//If there is a show tab, and this element matches.
+				if ($_GET['showTab'] && ('#' + $_GET['showTab']) == jQuery(this).attr('href')) {
+					//Activate.
+					jQuery(this).addClass('active');
+				}
+			});
+			
+			//rtrim comma.
+			hrefs	=	hrefs.replace(/,+$/,'');
+			
+			//If there is no show tab.
+			if (!$_GET['showTab']) {
+				//Hide all elements.
+				jQuery(hrefs).not($this.find('a:first').addClass('active').attr('href')).hide();
+			} else { 
+				//Scroll down.
+				jQuery('#' + $_GET['showTab']).ScrollTo();
+				
+				//Hide all but this element.
+				jQuery(hrefs).not('#' + $_GET['showTab']).hide();
+			}
+			
+			//For each nav element.
+			$this.find('a').click(function() {
+				//Remove the active class.
+				$this.find('a').not(this).removeClass('active');
+				
+				//Fade out the content. 
+				jQuery(hrefs).not(jQuery(this).attr('href')).hide();
+				
+				//Fade in the content. 
+				jQuery(jQuery(this).addClass('active').attr('href')).fadeIn(400);
+			});
+		});
+		
+		//For sliders.
+		jQuery('*[data-slide-id], *[data-slide-class]').each(function() {
+			//Get the id or class.
+			var	elements	=	(!jQuery(this).data('slide-id')) ? '.' + jQuery(this).data('slide-class') : '#' + jQuery(this).data('slide-id');
+			
+			//On click.
+			jQuery(this).click(function() {
+				//Toggle slide.
+				jQuery(elements).slideToggle(400);
+			});
+ 		});
 
 		//For each equalize.
 		jQuery('*[data-equalize]').each(function() {
@@ -161,8 +257,53 @@ require(["jquery"], function(jQuery) {
 		
 		//Equalize heights.
 		jQuery('.equalize').equalizeHeights();
+		
+		//If scrollto is allowed.
+		if (!jQuery('html').data('no-scroll')) {
+			//For each scrollto.
+			jQuery('a').each(function() { 
+				//Check if the hash is the first element.
+				if (jQuery(this).attr('href').substring(0, 1) === '#') {
+					//On click.
+					jQuery(this).click(function(e) {
+						//Get the element to scroll to.
+						var	ele	=	jQuery(this).attr('href');
+						
+						//Scroll to the element.
+						jQuery(ele).ScrollTo();
+						
+						//Prevent default functionality.
+						e.preventDefault();
+						
+						//Return false.
+						return false;
+					});
+				}
+			});
+		}
+		
+		//If there is history.
+		if (jQuery('html').data('history')) {
+			
+		}
+		
+		//Keystrokes.
+		jQuery(document).keydown(function (e) {
+			//Switch key movements.
+			switch(e.which) {
+
+			}
+		});
 	});
 });
+
+/**
+ * Gets query parameters. 
+ * @author http://stackoverflow.com/a/439578
+ * @param e
+ * @returns {___anonymous6837_6838}
+ */
+function getQueryParams(e){e=e.split("+").join(" ");var t={},n,r=/[?&]?([^=]+)=([^&]*)/g;while(n=r.exec(e)){t[decodeURIComponent(n[1])]=decodeURIComponent(n[2])}return t}
 
 /**
  * Implements a slide change. 
